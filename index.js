2f0ee73d558a5dcf302971c50e30effc62c835ff
rafiul from office <rafiulofficial396@gmail.com>
2025-12-14 17:45:27 +0600

    feat: connect server to MongoDB Atlas database
    - Configured MongoDB Atlas connection
    - Established secure database connection using environment variables
    - Verified server connectivity with database

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
@@ -6,11 +8,41 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+async function run() {
+    try {
+
+        // Create Database and Collection
+
+        // Get all plants by fetching this API
+
+        // Get single plant to fatche this API
+
+        // Setup payment getway system using stripe
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir)
+
 app.get("/", async (req, res) => {
     res.send("Hello Chef")
 })
 
 app.listen(port, () => {
     console.log(`Running port is ${port}`);
-    
+
 })
